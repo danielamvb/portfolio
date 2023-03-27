994b101d9018f1744f435fa9fd852076eed1e727
Daniela Moreira <40210349@esmad.ipp.pt>
2023-03-27 18:51:53 +0100

    more things

--- assets/js/main.js
+++ assets/js/main.js
@@ -98,30 +98,32 @@
       }
     });
 
-    $('.video-popup').magnificPopup({
-        type: 'iframe', 
-        gallery:{
-          enabled:true,
-        }
-      });
-
-    
-    // $('.image-popup', '.video-popup').magnificPopup({
+    // $('.video-popup').magnificPopup({
+    //     type: 'iframe', 
     //     gallery:{
-    //         enabled:true
-    //       },
-    //     type: 'image',
-        
-    //     callbacks: {
-    //         elementParse: function(item) {
-    //             if(item.el.context.className == 'video-popup') {
-    //                 item.type = 'iframe'
-    //             } else {
-    //                 item.type = 'image'
-    //             }
-    //         }
-    //     },
+    //       enabled:true,
+    //     }
     //   });
+
+
+    $('.video-popup').magnificPopup({
+    type: 'iframe',
+    
+    iframe: {
+        markup: '<div class="mfp-iframe-scaler">'+
+                    '<div class="mfp-close"></div>'+
+                    '<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>'+
+                    '<div class="mfp-title">Some caption</div>'+
+                '</div>'
+    },
+    callbacks: {
+        markupParse: function(template, values, item) {
+        values.title = item.el.attr('title');
+        }
+    }
+    
+    
+    });
     
     
     //===== Back to top
